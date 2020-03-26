1ae26ce1e242f6a44e6d2673ddd6983c1d5a3aea
George-Stephen <yazz777stevie@gmail.com>
2020-03-26 16:41:30 +0300

    Auto-generated commit

--- app/user-entry.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UserEntry } from './user-entry';
-
-describe('UserEntry', () => {
-  it('should create an instance', () => {
-    expect(new UserEntry()).toBeTruthy();
-  });
-});
--- app/user-entry.ts
+++
@@ -1,4 +0,0 @@
-export class UserEntry {
-    constructor( public completeDate: Date,public id: number,public Title: string,public Description: string ,){
-      }
-}
